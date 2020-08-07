ef536b755293f264f050696fec30fa43a4b77801
Hiago <Hiago-Maitan>
2020-08-07 12:03:55 -0300

    Configurando o servidor portátil com json-server

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -1,23 +1,112 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/Carousel/components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '',
+    }
+
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+        //chave: nome, descricao
+        setValues({
+            ...values,
+            [chave]: valor, //nome: 'valor'
+        })
+    }
+
+    function handleChange(infosDoEvento) {
+        setValue(
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+        );
+    }
+
+    useEffect(() => {
+        console.log("Aloo");
+        const URL_TOP = 'http://localhost:8080/categorias';
+        fetch(URL_TOP)
+            .then(async (respostaDoServidor) => {
+                const resposta = await respostaDoServidor.json();
+                setCategorias([
+                    ...resposta,
+                ]);
+            });
+
+
+        setTimeout(() => {
+
+        });
+    }, []);
+
     return (
         <PageDefault>
-            <h1>Cadastro de Cadastro</h1>
+            <h1>Cadastro de Categoria: {values.nome}</h1>
 
-            <form>
-                <label>
-                    Nome Categoria:
-                    <input
-                        type="text"
-                    />
-                    <button>
-                        Cadastrar
-                    </button>
-                </label>
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    values
+                ]);
+
+                setValues(valoresIniciais);
+            }}>
+
+
+                <FormField
+                    label="Nome da categoria"
+                    type="text"
+                    name="nome"
+                    value={values.nome}
+                    onChange={handleChange}
+                />
+
+                <FormField
+                    label="Descrição"
+                    type="textarea"
+                    name="descricao"
+                    value={values.descricao}
+                    onChange={handleChange}
+                />
+
+                <FormField
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={values.cor}
+                    onChange={handleChange}
+                />
+
+                <Button>
+                    Cadastrar
+                </Button>
+
             </form>
+
+            {categorias.length == 0 && (
+                <div>
+                    {/* Carregando */}
+                    Loading...
+                </div>
+            )}
+
+            <ul>
+                {categorias.map((categorias) => {
+                    return (
+                        <li key={`${categorias.nome}`}>
+                            {categorias.nome}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para Home
